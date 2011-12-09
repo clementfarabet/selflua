9fb6c538eb03a6ab2a7a6d5679630752d4d5ab2f
Clement Farabet <clement.farabet@gmail.com>
2011-12-09 14:26:11 -0500

    Added cpp protect

--- selflua.h
+++ selflua.h
@@ -4,6 +4,10 @@
 #include <lualib.h>
 #include <lauxlib.h>
 
+#ifdef __cplusplus 
+extern "C" {
+#endif
+
 extern lua_State *sl_stack;
 
 void sl_init(void);
@@ -12,3 +16,7 @@ int sl_dofile(const char *file);
 int sl_dostring(const char *string);
 lua_State * sl_getstack(void);
 void sl_callfunc(const char *funcname, int nargs, int nouts);
+
+#ifdef __cplusplus 
+}
+#endif
